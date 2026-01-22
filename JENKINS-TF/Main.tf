ea4b83cf079996bc5878d6968ce1e2e1f653d568
Neha C <165931219+cneha1014@users.noreply.github.com>
2026-01-22 17:01:33 +0530

    Update Main.tf

--- JENKINS-TF/Main.tf
+++ JENKINS-TF/Main.tf
@@ -31,12 +31,11 @@ resource "aws_security_group" "Jenkins-sg" {
 
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0f5ee92e2d63afc18"
-  instance_type          = "t2.large"
-  key_name               = "Mumbai"
+  ami                    = "ami-0786adace1541ca80"
+  instance_type          = "m7i-flex.large"
+  key_name               = "amazon-FE"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
-  user_data              = templatefile("./install_jenkins.sh", {})
-
+  
   tags = {
     Name = "Jenkins-sonar"
   }
