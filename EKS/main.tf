7ca2804eb065b9589c84cda3e9ac9dee2698c5ad
Neha C <165931219+cneha1014@users.noreply.github.com>
2026-01-22 21:26:36 +0530

    Update main.tf

--- EKS/main.tf
+++ EKS/main.tf
@@ -92,15 +92,15 @@ resource "aws_eks_node_group" "example" {
   node_role_arn   = aws_iam_role.example1.arn
   subnet_ids      = data.aws_subnets.public.ids
 
+  ami_type       = "AL2_ARM_64"
+  instance_types = ["t4g.micro"]
+
   scaling_config {
     desired_size = 1
     max_size     = 2
     min_size     = 1
   }
-  instance_types = [" t3.small"]
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
     aws_iam_role_policy_attachment.example-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.example-AmazonEKS_CNI_Policy,
